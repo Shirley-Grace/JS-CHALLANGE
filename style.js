3bc056f7fc10a1393022f7021a71bc05f8f0a186
Shirley_Grace <giray.mungai@gmail.com>
2024-04-04 10:01:40 +0300

    buttons

---
+++ style.js
@@ -0,0 +1,12 @@
+const hello = document.getElementsByClassName("button1")
+const back = document.getElementsByClassName("button2")
+const change = document.getElementById("intro")
+
+
+ hello.addEventListener("click", function(){
+change.style.display = "none"
+ })
+back.addEventListener("click",function(){
+
+})
+
