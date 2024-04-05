ec8304218d4e78c38e8a52676e518906a2b8b6b6
Shirley_Grace <giray.mungai@gmail.com>
2024-04-05 10:27:51 +0300

    sych

---
+++ sych$asych/sych.js
@@ -0,0 +1,8 @@
+function greet(name) {
+    return "hello"+name;    
+}
+console.log("Berfore return call");
+console.log(greet("Tom"));
+console.log(greet("Dick"));
+console.log(greet("Harry"));
+console.log("end callback");
