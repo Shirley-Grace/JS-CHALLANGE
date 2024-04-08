f6e4756fcfcc1e462ec4374bf2143b94f61fa50b
Shirley_Grace <giray.mungai@gmail.com>
2024-04-08 07:48:16 +0300

    todo

---
+++ Challanges/style.js
@@ -0,0 +1,14 @@
+// // // event handler
+
+// function change() {
+//     document.getElementById("intro").innerHTML="Hello World!!";
+    
+// }
+
+// event listener
+const btn1 = document.getElementById("btn1")
+const hello = document.getElementById("intro")
+
+btn1.addEventListener("click", () => {
+    return "Hello World!!";
+});
