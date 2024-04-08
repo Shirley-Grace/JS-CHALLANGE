3d568329efb7c4822de9656033e57ff33bb1f881
Shirley_Grace <giray.mungai@gmail.com>
2024-04-08 09:05:28 +0300

    list challange

---
+++ Challange1/style.js
@@ -0,0 +1,18 @@
+// // event handler
+
+function change() {
+    document.getElementById("intro").innerHTML="Hello World!!";
+    
+}
+function back(){
+    location.reload();
+
+}
+
+// // event listener
+// const btn1 = document.getElementById("btn1")
+// const hello = document.getElementById("intro")
+
+// btn1.addEventListener("click", () => {
+//     return "Hello World!!";
+// });
